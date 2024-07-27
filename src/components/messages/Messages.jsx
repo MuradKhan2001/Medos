0b6812841b769aa63c7e81c9b3a9e8af2b3967d8
Murodxon <94508619+MuradKhan2001@users.noreply.github.com>
2024-07-27 16:55:46 +0500

    new

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -44,25 +44,7 @@ const Messages = () => {
                                 {item.phone}
                             </div>
                             <div className="des">
-                                {item.text} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores,
-                                consequuntur dolorem ea illum incidunt ipsam iste nisi perferendis quod quos ratione
-                                similique tempora tempore. Assumenda at aut, consectetur consequuntur delectus dolorum
-                                et explicabo fugit ipsa ipsam labore magni mollitia necessitatibus nihil nostrum odio
-                                recusandae reiciendis sed ut voluptatibus. A culpa deserunt dolorem doloremque, dolores
-                                ex nesciunt, quam quia quibusdam recusandae sequi veniam voluptas voluptatum. Ab dolore
-                                et exercitationem minus qui quibusdam reprehenderit veritatis vitae. Accusamus aliquam
-                                atque cupiditate debitis eaque eos et ex, expedita facere incidunt itaque labore laborum
-                                magni minima, modi odio praesentium qui quia quos repudiandae rerum sapiente sequi
-                                similique, sunt totam vel voluptates. Accusantium animi architecto consequuntur cumque,
-                                cupiditate ea eos harum neque odio quae. Aliquam amet autem optio pariatur quam quasi
-                                rerum voluptate. Architecto aspernatur eveniet excepturi impedit iusto laudantium
-                                maiores minima. Corporis dignissimos doloribus, esse laborum mollitia obcaecati sed
-                                similique sint, sit, soluta tenetur totam vel. Consectetur consequuntur corporis
-                                deserunt dicta dignissimos error eveniet ex fuga harum illo illum inventore, ipsam iusto
-                                labore mollitia nesciunt non odio odit officiis omnis praesentium provident qui
-                                reprehenderit saepe temporibus veniam, voluptas. Deleniti doloremque eius enim maiores
-                                minima nesciunt perferendis totam? Amet dolore expedita iure nesciunt perspiciatis
-                                reiciendis repellendus.
+                                {item.text}
                             </div>
                             <div className="time">
                                 <span>Qabul kuni:</span>
