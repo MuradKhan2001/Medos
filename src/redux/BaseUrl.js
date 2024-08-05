035e4731f31e8c0e4287ba5899a6b60af0ea2a6c
Murodxon <94508619+MuradKhan2001@users.noreply.github.com>
2024-08-05 18:51:05 +0500

    fix base url

--- src/redux/BaseUrl.js
+++ src/redux/BaseUrl.js
@@ -2,7 +2,7 @@ import {createSlice} from "@reduxjs/toolkit"
 
 export const baseUrl = createSlice({
     name: "baseUrl",
-    initialState: {data: "http://192.168.43.59:8000/api/v1/"},
+    initialState: {data: "https://api.medos.uz/api/v1/"},
 });
 
 export default baseUrl.reducer
